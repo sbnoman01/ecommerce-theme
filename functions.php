6e502cd79aec5c1a9057c89dfbe093919a031eae
sbnoman01 <sbnoman27@gmail.com>
2021-11-28 22:52:14 +0600

    single product page

--- functions.php
+++ functions.php
@@ -113,6 +113,63 @@ function display_percentage_on_sale_badge( $html, $post, $product ) {
   return '<div class="product__discount__percent">' . esc_html__( '-', 'woocommerce' ) . ' '. $percentage . '</div>'; // If needed then change or remove "up to -" text
 }
 
+//  add to card plus minus 
+add_action( 'woocommerce_after_add_to_cart_quantity', 'ts_quantity_plus_sign' );
+ 
+function ts_quantity_plus_sign() {
+   echo '<button type="button" class="plus" >+</button>';
+}
+ 
+add_action( 'woocommerce_before_add_to_cart_quantity', 'ts_quantity_minus_sign' );
+
+function ts_quantity_minus_sign() {
+   echo '<button type="button" class="minus" >-</button>';
+}
+ 
+add_action( 'wp_footer', 'ts_quantity_plus_minus' );
+ 
+function ts_quantity_plus_minus() {
+   // To run this on the single product page
+   if ( ! is_product() ) return;
+   ?>
+   <script type="text/javascript">
+          
+      jQuery(document).ready(function($){   
+          
+            $('form.cart').on( 'click', 'button.plus, button.minus', function() {
+ 
+            // Get current quantity values
+            var qty = $( this ).closest( 'form.cart' ).find( '.qty' );
+            var val   = parseFloat(qty.val());
+            var max = parseFloat(qty.attr( 'max' ));
+            var min = parseFloat(qty.attr( 'min' ));
+            var step = parseFloat(qty.attr( 'step' ));
+ 
+            // Change the value if plus or minus
+            if ( $( this ).is( '.plus' ) ) {
+               if ( max && ( max <= val ) ) {
+                  qty.val( max );
+               } 
+            else {
+               qty.val( val + step );
+                 }
+            } 
+            else {
+               if ( min && ( min >= val ) ) {
+                  qty.val( min );
+               } 
+               else if ( val > 1 ) {
+                  qty.val( val - step );
+               }
+            }
+             
+         });
+          
+      });
+          
+   </script>
+   <?php
+}
 
 
 ?>
